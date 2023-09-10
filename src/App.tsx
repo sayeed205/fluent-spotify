b54e9907820cabc4518f202ee2774e66e0988496
Hitarashi <sayeed205@gmail.com>
2023-09-10 07:42:58 +0530

    Add Initial UI from my other project

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import RootLayout from '@/layouts/root-layout';
+import {
+    ActionFunction,
+    LoaderFunction,
+    RouterProvider,
+    createBrowserRouter,
+} from 'react-router-dom';
+
+interface RouteCommon {
+    loader?: LoaderFunction;
+    action?: ActionFunction;
+    ErrorBoundary?: React.ComponentType<any>;
+}
+
+interface IRoute extends RouteCommon {
+    path: string;
+    Element: React.ComponentType<any>;
+}
+
+interface Pages {
+    [key: string]: {
+        default: React.ComponentType<any>;
+    } & RouteCommon;
+}
+
+const pages: Pages = import.meta.glob('./pages/**/*.tsx', { eager: true });
+
+const routes: IRoute[] = [];
+for (const path of Object.keys(pages)) {
+    const fileName = path.match(/\.\/pages\/(.*)\.tsx$/)?.[1];
+    if (!fileName) {
+        continue;
+    }
+
+    const normalizedPathName = fileName.includes('$')
+        ? fileName.replace('$', ':')
+        : fileName.replace(/\/index/, '');
+
+    routes.push({
+        path:
+            fileName === 'index' ? '/' : `/${normalizedPathName.toLowerCase()}`,
+        Element: pages[path].default,
+        loader: pages[path]?.loader as LoaderFunction | undefined,
+        action: pages[path]?.action as ActionFunction | undefined,
+        ErrorBoundary: pages[path]?.ErrorBoundary,
+    });
+}
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <RootLayout />,
+        children: routes.map(({ Element, ErrorBoundary, ...rest }) => ({
+            ...rest,
+            element: <Element />,
+            ...(ErrorBoundary && { errorElement: <ErrorBoundary /> }),
+        })),
+    },
+]);
+
+const App = () => <RouterProvider router={router} />;
+
+export default App;
---
+++ src/components/ui/window-bar.tsx
@@ -0,0 +1,74 @@
+import { appWindow } from '@tauri-apps/api/window';
+import { useState } from 'react';
+
+import { Icons } from '@/components/icons';
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+const controlButtons = [
+    {
+        name: 'minimize',
+        icon: <Icons.minimize />,
+    },
+    {
+        name: 'maximize',
+        icon: <Icons.maximize />,
+    },
+    {
+        name: 'close',
+        icon: <Icons.close />,
+    },
+];
+
+export default function WindowBar({ muted }: { muted?: string[] }) {
+    const [isMaximized, setIsMaximized] = useState<boolean>(false);
+
+    const handleClick = (name: string) => {
+        switch (name) {
+            case 'minimize':
+                appWindow.minimize();
+                break;
+            case 'maximize':
+                appWindow.toggleMaximize();
+                setIsMaximized(!isMaximized);
+                break;
+            case 'close':
+                appWindow.close();
+                break;
+        }
+    };
+
+    appWindow.isMaximized().then(isMaximized => setIsMaximized(isMaximized));
+
+    return (
+        <div
+            className='h-7 select-none flex left-0 right-0 justify-end gap-[1px] m-2 '
+            data-tauri-drag-region
+        >
+            {controlButtons.map(button => (
+                <div
+                    key={button.name}
+                    className='flex items-center align-middle text-center'
+                >
+                    <div
+                        className={cn(
+                            buttonVariants({ variant: 'ghost' }),
+                            muted?.includes(button.name) &&
+                                'text-gray-500 hover:text-gray-500 bg-transparent hover:bg-transparent'
+                        )} // todo)) Need to add bg-red for close button while hovered
+                        onClick={() => {
+                            muted?.includes(button.name) ||
+                                handleClick(button.name);
+                        }}
+                    >
+                        {isMaximized && button.name === 'maximize' ? (
+                            <Icons.restore />
+                        ) : (
+                            button.icon
+                        )}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
